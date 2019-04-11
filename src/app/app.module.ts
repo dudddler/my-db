a51c4abfe09f92c4e99eeed8ea743fc0ea3252f8
dudddler <49159659+dudddler@users.noreply.github.com>
2019-04-11 12:25:07 -0700

    2
    Second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material';
 import { IconsModule } from 'angular-bootstrap-md';
-
+import { ChartsModule, WavesModule } from 'angular-bootstrap-md'
+import { CollapseModule } from 'angular-bootstrap-md'
+import { MatExpansionModule } from '@angular/material/expansion'
 
 
 @NgModule({
@@ -20,7 +22,11 @@ import { IconsModule } from 'angular-bootstrap-md';
     MDBBootstrapModule.forRoot(),
     BrowserAnimationsModule,
     MatIconModule,
-    IconsModule
+    IconsModule,
+    ChartsModule,
+    WavesModule.forRoot(),
+    CollapseModule.forRoot(),
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  //This is code that controls the charts data
+  //line chart  
+  public chartType: string = 'line';
+
+  public chartDatasets: Array<any> = [
+    { data: [5.5, 5.9, 6.2, 6.4, 7.2, 7.9], label: 'Investor Yieled' }
+  ];
+
+  public chartLabels: Array<any> = ['2013', '2014', '2015', '2016', '2017', 'YTD 2018'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+
+  //colapse code
+  panelOpenState = false;  
+
+
 
   constructor() { }
 
